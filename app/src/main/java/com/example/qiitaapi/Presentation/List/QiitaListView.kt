0e75b1211d7ff2c369b8c4f5061f2cc793d4ed60
Wyutaka <tihiro.haul@gmail.com>
2019-12-10 09:53:52 +0900

    lifecycleScopeSupportを追加

--- app/src/main/java/com/example/qiitaapi/Presentation/List/QiitaListView.kt
+++ app/src/main/java/com/example/qiitaapi/Presentation/List/QiitaListView.kt
@@ -1,5 +1,9 @@
 package com.example.qiitaapi.Presentation.List
 
-interface QiitaListView {
+import LifecycleScopeSupport
+import android.view.View
 
+interface QiitaListView : LifecycleScopeSupport{
+    fun onItemClick(view: View, position: Int)
+    fun showDetail(view: View, position: Int)
 }
